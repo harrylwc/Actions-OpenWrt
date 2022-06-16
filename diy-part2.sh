362d7d77833686e45c51a2997a163f918f0d1492
Harry Lau <harrylwc@gmai.com>
2022-06-16 14:16:31 +0800

    修改：     diy-part2.sh 	新檔案：   patches/revert_set_default_root.patch

--- diy-part2.sh
+++ diy-part2.sh
@@ -54,7 +54,7 @@ mv $GITHUB_WORKSPACE/patches/800-custom-hk.patch $GITHUB_WORKSPACE/x-wrt/package
 cp $GITHUB_WORKSPACE/patches/*.patch $GITHUB_WORKSPACE/x-wrt/
 cd $GITHUB_WORKSPACE/x-wrt/
 git apply --ignore-space-change --ignore-whitespace *.patch
-git revert 789a6b7
+#git revert 789a6b7
 #cp $GITHUB_WORKSPACE/lede/package/kernel/linux/modules/fs.mk $GITHUB_WORKSPACE/x-wrt/package/kernel/linux/modules/
 #cp $GITHUB_WORKSPACE/lede/target/linux/generic/backport-5.10/35?*.patch $GITHUB_WORKSPACE/x-wrt/target/linux/generic/backport-5.10/
 exit 0
