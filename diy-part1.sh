6a2a381d80ab497be27ca3bdb935582c6f332d78
harrylwc <harrylwc@gmail.com>
2023-03-16 00:09:53 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -21,9 +21,9 @@ sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #rm -r x-wrt/package/lean/n2n*
 cd x-wrt 
 git pull
-git reset --hard 22.10_b202303121313
-sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
+#git reset --hard 22.10_b202303121313
+#sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+#sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 #git checkout 90d9cb4
 #git checkout 7832be9
 #git checkout 166ab6f90eef9e08b1d5f45fe5464ecd2194b2e3 
