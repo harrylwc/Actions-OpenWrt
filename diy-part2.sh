cbf01fe013e6f06d5d28c3cc188e56a7b065f23e
Harry Lau <harrylwc@gmail.com>
2023-09-20 03:56:48 +0800

    modified:   build_r619ac2.yaml 	modified:   build_r619ac3.yaml 	modified:   build_wrt1200ac2.yaml 	modified:   ../../diy-part2.sh 	modified:   ../../myconfig/config.r619ac.0 	modified:   ../../myconfig/config.r619ac.2 	modified:   ../../myconfig/config.r619ac.3

--- diy-part2.sh
+++ diy-part2.sh
@@ -22,7 +22,7 @@ git clone https://github.com/tvheadend/tvheadend.git
 cp -r $GITHUB_WORKSPACE/x-wrt/tvheadend/.git $GITHUB_WORKSPACE/x-wrt/package/tvheadend/files/
 sed -i 's/KERNEL_PATCHVER:=5.10/KERNEL_PATCHVER:=5.15/g' target/linux/ramips/Makefile
 sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' target/linux/mvebu/Makefile
-#sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' target/linux/ipq40xx/Makefile 
+sed -i 's/KERNEL_PATCHVER:=5.15/KERNEL_PATCHVER:=6.1/g' target/linux/ipq40xx/Makefile 
 rm -r feeds/packages/multimedia/tvheadend
 #tar xvf /home/runner/work/Actions-OpenWrt/Actions-OpenWrt/tvheadend.tar -C /home/runner/work/Actions-OpenWrt/Actions-OpenWrt/openwrt/feeds/packages/multimedia/
 #rm -r feeds/packages/multimedia/ffmpeg
