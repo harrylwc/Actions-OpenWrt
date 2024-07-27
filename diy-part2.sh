a34fee376e4a87b5f14013f12c79b238f9ff5d4a
Harry Lau <harrylwc@gmail.com>
2024-07-27 09:52:39 +0800

    modified:   ../diy-part2.sh 	modified:   config.newifi-d2.0 	modified:   config.newifi-d2.0.gcc14 	modified:   config.newifi-d2.2 	modified:   config.newifi-d2.2.gcc14 	modified:   config.ramips.2 	modified:   config.ramips.2.gcc13 	modified:   config.ramips.2.gcc14 	deleted:    ../patches/301-curl-mbedtls.patch

--- diy-part2.sh
+++ diy-part2.sh
@@ -37,7 +37,7 @@ ln -s /home/runner/work/Actions-OpenWrt/Actions-OpenWrt/lede/feeds/luci/applicat
 #rm -r $GITHUB_WORKSPACE/x-wrt/package/libs/gettext-full
 #cp -r $GITHUB_WORKSPACE/gettext-full $GITHUB_WORKSPACE/x-wrt/package/libs/
 #cp $GITHUB_WORKSPACE/patches/socat.Makefile  $GITHUB_WORKSPACE/x-wrt/feeds/packages/net/socat/Makefile
-cp $GITHUB_WORKSPACE/patches/30*-curl-mbed*.patch $GITHUB_WORKSPACE/x-wrt/feeds/packages/net/curl/patches/
+#cp $GITHUB_WORKSPACE/patches/30*-curl-mbed*.patch $GITHUB_WORKSPACE/x-wrt/feeds/packages/net/curl/patches/
 cp $GITHUB_WORKSPACE/patches/Makefile.rust  $GITHUB_WORKSPACE/x-wrt/package/feeds/packages/rust/Makefile
 #cp $GITHUB_WORKSPACE/patches/002-Support-POSIX-basename-from-musl-libc.patch $GITHUB_WORKSPACE/x-wrt/feeds/packages/utils/tini/patches/
 #mkdir $GITHUB_WORKSPACE/x-wrt/feeds/packages/utils/rtty/patches
