74afe527ceba5feabbb60784ecf3b4e7479acf7f
Harry Lau <harrylwc@gmail.com>
2024-10-05 19:25:46 +0800

    modified:   diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,6 +20,7 @@ sed -i '$a src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf
 sed -i '$a src-git small https://github.com/kenzok8/small' feeds.conf.default
 git pull
 git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+rm -r x-wrt/feeds/luci/applications/luci-app-filebrowser/
 ./scripts/feeds update -a
 ./scripts/feeds install -f -a
 rm -rf package/openwrt-packages/luci-app-wechatpush
