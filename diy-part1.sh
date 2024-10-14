bb67af46803f1471f79eb3a8bf0cefe4d0dbd34c
harrylwc <harrylwc@gmail.com>
2024-10-14 17:34:14 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -3,12 +3,14 @@ git clone https://github.com/coolsnowwolf/lede lede
 lede/scripts/feeds update -a
 
 cd x-wrt
-git revert eb05baff7fb330de43632d638c34ba544f472935
-git revert 7eb6bf1ac9798f07577f7c2e0ef12f622efc6315
-git revert 8766a92766962f7267bf44851aaacd3588249473
-git revert 3e668c6d02ed637620bde27c66c0bdeedf18f54f
-git revert 9cecf2b16e0ea8560e50ef6719938bd80b963704
-
+git revert eb05baff7fb330de43632d638c34ba544f472935 >result.txt
+git revert 7eb6bf1ac9798f07577f7c2e0ef12f622efc6315>>result.txt
+git revert 8766a92766962f7267bf44851aaacd3588249473>>result.txt
+git revert 3e668c6d02ed637620bde27c66c0bdeedf18f54f>>result.txt
+git revert 9cecf2b16e0ea8560e50ef6719938bd80b963704>>result.txt
+echo "git revert result====="
+cat result.txt
+echo "==========="
 #rm target/linux/ramips/dts/mt7620a.dtsi
 #rm target/linux/ramips/dts/mt7620n.dtsi
 #rm target/linux/ramips/mt7620/target.mk
