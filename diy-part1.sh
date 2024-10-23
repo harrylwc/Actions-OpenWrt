fd0ac7651c0df7a52442e996e8373aa5df7dd74f
Harry Lau <harrylwc@gmail.com>
2024-10-23 08:02:11 +0800

    modified:   diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,8 +13,8 @@ echo "Job name is $GITHUB_JOB"
 #if echo $GITHUB_WORKFLOW_REF |grep openwrt
 #then
       # git checkout 424210b #KERNEL 5.15.81
-       git checkout 22.03_b202210282250 #5.15.74
-	#git checkout 9.0_b202110300939 
+#git checkout 22.03_b202210282250 #5.15.74
+git checkout 9.0_b202110300939 #5.4.155 
 
 #git checkout 0eed96ca5d #kernel 5.4.152
      #   0a2ed285e4
