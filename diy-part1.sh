7cb0d7fbef8e6f140c54f9416e938d3e3bdd7376
Harry Lau <harrylwc@gmail.com>
2024-10-24 13:44:18 +0800

    modified:   diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,8 +12,13 @@ echo "Job name is $GITHUB_JOB"
 
 #if echo $GITHUB_WORKFLOW_REF |grep openwrt
 #then
-      # git checkout 424210b #KERNEL 5.15.81
-   #  git checkout 0eed96ca5d #kernel 5.4.152
+
+# Linux version 5.4.152 (runner@fv-az1538-855) (gcc version 11.2.0 (OpenWrt GCC 11.2.0 r17755-0eed96ca5d8)) #0 Wed Oct 13 20:55:45 2021
+# Powered by LuCI branch (git-21.286.81213-cd11ef6) / OpenWrt SNAPSHOT r17755-0eed96ca5d8
+
+      
+# git checkout 424210b #KERNEL 5.15.81
+   #git checkout 0eed96ca5d #kernel 5.4.152
      #   0a2ed285e4
 #else
         #git checkout 610ea1b9994
