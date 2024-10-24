7101ab8014664a5622724ef16358983a2e1f0aac
Harry Lau <harrylwc@gmail.com>
2024-10-24 15:03:57 +0800

    modified:   diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -20,9 +20,10 @@ echo "Job name is $GITHUB_JOB"
 git checkout 9.0_b202110300939 #5.4.155
 
 #git checkout v21.02.7
+git checkout v21.02.1 # 5.4.154 
 #git checkout 0eed96ca5d8 
-#cp $GITHUB_WORKSPACE/myconfig/config.ramips.2.kernel5.4 cp $GITHUB_WORKSPACE/myconfig/config.ramips.2.openwrt
-#cp $GITHUB_WORKSPACE/myconfig/config.ramips.2.kernel5.4 x-wrt/.config
+cp $GITHUB_WORKSPACE/myconfig/config.ramips.2.kernel5.4 cp $GITHUB_WORKSPACE/myconfig/config.ramips.2.openwrt
+cp $GITHUB_WORKSPACE/myconfig/config.ramips.2.kernel5.4 x-wrt/.config
 
 
 #git checkout 0eed96ca5d #kernel 5.4.152
