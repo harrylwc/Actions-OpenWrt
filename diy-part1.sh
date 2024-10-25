004bf73f4d6cc62e98cf5531671d656ea2469fb4
Harry Lau <harrylwc@gmail.com>
2024-10-25 15:24:16 +0800

    modified:   diy-part1.sh 	modified:   diy-part2.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -19,12 +19,11 @@ echo "Job name is $GITHUB_JOB"
 
 #git checkout 9.0_b202110300939 #5.4.155
 
-git checkout v21.02.7; mv $GITHUB_WORKSPACE/patches/600-custom-change-txpower-and-dfs_v21.02.7.patch $GITHUB_WORKSPACE/patches/600-custom-change-txpower-and-dfs.patch 
-#git checkout v21.02.1 ;mv $GITHUB_WORKSPACE/patches/600-custom-change-txpower-and-dfs_v21.02.1.patch $GITHUB_WORKSPACE/patches/600-custom-change-txpower-and-dfs.patch;cp $GITHUB_WORKSPACE/myconfig/config.ramips.v21.02.1 $GITHUB_WORKSPACE/myconfig/config.ramips.2.kernel5.4 ; cp $GITHUB_WORKSPACE/patches/Makefile.ccache.4.2.1 $GITHUB_WORKSPACE/x-wrt/tools/ccache/Makefile 
-
+#git checkout v21.02.7 ; cp $GITHUB_WORKSPACE/patches/600-custom-change-txpower-and-dfs_kernel_v21.02.7.patch $GITHUB_WORKSPACE/patches/600-custom-change-txpower-and-dfs_kernel5.4.patch
+#git checkout v21.02.1 # 5.4.154;cp $GITHUB_WORKSPACE/myconfig/config.ramips.v21.02.1 $GITHUB_WORKSPACE/myconfig/config.ramips.2.kernel5.4
 #git checkout 0eed96ca5d8 
-cp $GITHUB_WORKSPACE/myconfig/config.ramips.2.kernel5.4 $GITHUB_WORKSPACE/myconfig/config.ramips.2.openwrt
-cp $GITHUB_WORKSPACE/myconfig/config.ramips.2.kernel5.4 x-wrt/.config
+#cp $GITHUB_WORKSPACE/myconfig/config.ramips.2.kernel5.4 $GITHUB_WORKSPACE/myconfig/config.ramips.2.openwrt
+#cp $GITHUB_WORKSPACE/myconfig/config.ramips.2.kernel5.4 x-wrt/.config
 
 
 #git checkout 0eed96ca5d #kernel 5.4.152
@@ -37,26 +36,35 @@ cp $GITHUB_WORKSPACE/myconfig/config.ramips.2.kernel5.4 x-wrt/.config
 #cd luci; git checkout ea0e494;cd $GITHUB_WORKSPACE/x-wrt/ 
 
 
-#wget https://github.com/openwrt/openwrt/commit/eeb59f87a18843c541277a6236b4f2b6e41889e9 -O 0.patch
-#wget https://github.com/openwrt/openwrt/commit/eb05baff7fb330de43632d638c34ba544f472935.patch -O 1.patch
-#wget https://github.com/openwrt/openwrt/commit/7eb6bf1ac9798f07577f7c2e0ef12f622efc6315.patch -O 2.patch
-#wget https://github.com/openwrt/openwrt/commit/8766a92766962f7267bf44851aaacd3588249473.patch -O 3.patch
-#wget https://github.com/openwrt/openwrt/commit/3e668c6d02ed637620bde27c66c0bdeedf18f54f.patch -O 4.patch
-#wget https://github.com/openwrt/openwrt/commit/9cecf2b16e0ea8560e50ef6719938bd80b963704.patch -O 5.patch
+cd $GITHUB_WORKSPACE/x-wrt/feeds/packages/
+wget https://github.com/x-wrt/packages/commit/d4d6515dd40e43138d0a6b8cc046b8e89ce1cf6c.patch -O 6.patch
+wget https://github.com/x-wrt/packages/commit/58c11c1422fdb4bff4fed08757b8f374d346f974.patch -O 7.patch
+wget https://github.com/x-wrt/packages/commit/9be1307e2daba23b023c8ce652e968452f0f697c.patch -O 8.patch
+wget https://github.com/x-wrt/packages/commit/ad9d5a3eef961dcf2bef0e5486898c823786e53c.patch -O 9.patch
 
 
-#wget https://github.com/openwrt/openwrt/commit/eb05baff7fb330de43632d638c34ba544f472935.patch -O 1.patch
-#wget https://github.com/openwrt/openwrt/commit/7eb6bf1ac9798f07577f7c2e0ef12f622efc6315.patch -O 2.patch
-#wget https://github.com/openwrt/openwrt/commit/8766a92766962f7267bf44851aaacd3588249473.patch -O 3.patch
-#wget https://github.com/openwrt/openwrt/commit/3e668c6d02ed637620bde27c66c0bdeedf18f54f.patch -O 4.patch
-#wget https://github.com/openwrt/openwrt/commit/9cecf2b16e0ea8560e50ef6719938bd80b963704.patch -O 5.patch
 
-#git apply -R --ignore-space-change 0.patch
-#git apply -R --ignore-space-change 1.patch
-#git apply -R --ignore-space-change 2.patch
-#git apply -R --ignore-space-change 3.patch
-#git apply -R --ignore-space-change 4.patch
-#git apply -R --ignore-space-change 5.patch
+git apply -R --ignore-space-change 6.patch
+git apply -R --ignore-space-change 7.patch
+git apply -R --ignore-space-change 8.patch
+git apply -R --ignore-space-change 9.patch
+
+cd $GITHUB_WORKSPACE/x-wrt/
+
+
+wget https://github.com/openwrt/openwrt/commit/eeb59f87a18843c541277a6236b4f2b6e41889e9.patch -O 0.patch
+wget https://github.com/openwrt/openwrt/commit/eb05baff7fb330de43632d638c34ba544f472935.patch -O 1.patch
+wget https://github.com/openwrt/openwrt/commit/7eb6bf1ac9798f07577f7c2e0ef12f622efc6315.patch -O 2.patch
+wget https://github.com/openwrt/openwrt/commit/8766a92766962f7267bf44851aaacd3588249473.patch -O 3.patch
+wget https://github.com/openwrt/openwrt/commit/3e668c6d02ed637620bde27c66c0bdeedf18f54f.patch -O 4.patch
+wget https://github.com/openwrt/openwrt/commit/9cecf2b16e0ea8560e50ef6719938bd80b963704.patch -O 5.patch
+
+git apply -R --ignore-space-change 0.patch
+git apply -R --ignore-space-change 1.patch
+git apply -R --ignore-space-change 2.patch
+git apply -R --ignore-space-change 3.patch
+git apply -R --ignore-space-change 4.patch
+git apply -R --ignore-space-change 5.patch
 
 #cat package/network/services/ppp/Makefile|grep PKG_RELEASE_VERSION:=
 
@@ -100,9 +108,7 @@ cp -r $GITHUB_WORKSPACE/package/luci-app-shadowsocks-libev $GITHUB_WORKSPACE/x-w
 #cd x-wrt/feeds;mv luci luci.bak;git clone https://github.com/x-wrt/luci
 #cd luci; git checkout ea0e494;cd $GITHUB_WORKSPACE/x-wrt/
 
-#wget https://github.com/x-wrt/packages/commit/ad9d5a3eef961dcf2bef0e5486898c823786e53c.patch -O feeds/packages/6.patch
-#cd $GITHUB_WORKSPACE/x-wrt/feeds/packages/                                                                                                
-#git apply -R --ignore-space-change 6.patch                                                                                                
+
 cd $GITHUB_WORKSPACE/x-wrt    
 
 rm -rf package/openwrt-packages/luci-app-wechatpush
