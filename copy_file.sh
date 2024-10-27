cd99b3b65f9b3a994aef6fa6a03bc3775be7b7d6
Harry Lau <harrylwc@gmail.com>
2024-10-27 10:06:53 +0800

    modified:   copy_file.sh

--- copy_file.sh
+++ copy_file.sh
@@ -1,1 +1,2 @@
-curl -i -X POST r619ac.ubddns.org:8080/store.json --data-binary "@"$1
+#curl -i -X POST r619ac.ubddns.org:8080/store.json --data-binary "@"$1
+curl -X POST http://r619ac.ubddns.org:8080/upload -F 'files=@'$1
