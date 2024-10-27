6b5c812be3cb4e11ba6dcf514e6bbba6228b6857
Harry Lau <harrylwc@gmail.com>
2024-10-27 23:13:09 +0800

    modified:   diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,11 +18,11 @@ echo "Job name is $GITHUB_JOB"
 #git1 checkout 22.03_b202210282250 #5.15.74
 #git1 checkout 9.0_b202110300939 #5.4.155
 
-git checkout 9.0_b202110300939 #5.4.155
+#git checkout 9.0_b202110300939 #5.4.155
 #cp $GITHUB_WORKSPACE/myconfig/config.ramips.2.x-wrt.kernel5.4 $GITHUB_WORKSPACE/myconfig/config.ramips.2.x-wrt
 #cp $GITHUB_WORKSPACE/myconfig/config.ramips.2.x-wrt.kernel5.4 $GITHUB_WORKSPACE/myconfig/config.ramips.2.kernel5.4
 
-#git checkout v21.02.7;cp $GITHUB_WORKSPACE/myconfig/config.ramips.v21.02.7 $GITHUB_WORKSPACE/myconfig/config.ramips.2.kernel5.4; mv $GITHUB_WORKSPACE/patches/600-custom-change-txpower-and-dfs_v21.02.7.patch $GITHUB_WORKSPACE/patches/600-custom-change-txpower-and-dfs.patch
+git checkout v21.02.7;cp $GITHUB_WORKSPACE/myconfig/config.ramips.v21.02.7 $GITHUB_WORKSPACE/myconfig/config.ramips.2.kernel5.4; mv $GITHUB_WORKSPACE/patches/600-custom-change-txpower-and-dfs_v21.02.7.patch $GITHUB_WORKSPACE/patches/600-custom-change-txpower-and-dfs.patch
 #git checkout v21.02.1 ;mv $GITHUB_WORKSPACE/patches/600-custom-change-txpower-and-dfs_v21.02.1.patch $GITHUB_WORKSPACE/patches/600-custom-change-txpower-and-dfs.patch	;cp $GITHUB_WORKSPACE/myconfig/config.ramips.v21.02.1 $GITHUB_WORKSPACE/myconfig/config.ramips.2.kernel5.4 ; cp $GITHUB_WORKSPACE/patches/Makefile.ccache.4.2.1 $GITHUB_WORKSPACE/x-wrt/tools/ccache/Makefile
 
 #cp $GITHUB_WORKSPACE/myconfig/config.ramips.2.kernel5.4 $GITHUB_WORKSPACE/myconfig/config.ramips.2.openwrt
