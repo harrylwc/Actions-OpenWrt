c56910c0ce99a74be83ad8ea53a9776d6306db43
Harry Lau <harrylwc@gmail.com>
2024-10-27 19:52:11 +0800

    modified:   diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -29,7 +29,7 @@ git checkout v21.02.7; mv $GITHUB_WORKSPACE/patches/600-custom-change-txpower-an
 #git checkout v21.02.1 ;mv $GITHUB_WORKSPACE/patches/600-custom-change-txpower-and-dfs_v21.02.1.patch $GITHUB_WORKSPACE/patches/600-custom-change-txpower-and-dfs.patch;cp $GITHUB_WORKSPACE/myconfig/config.ramips.v21.02.1 $GITHUB_WORKSPACE/;cp myconfig/config.ramips.2.kernel5.4 ; cp $GITHUB_WORKSPACE/patches/Makefile.ccache.4.2.1 $GITHUB_WORKSPACE/x-wrt/tools/ccache/Makefile 
 
 #git checkout 0eed96ca5d8 
-cp $GITHUB_WORKSPACE/myconfig/config.ramips.2.kernel5.4 $GITHUB_WORKSPACE/myconfig/config.ramips.2.openwrt
+#cp $GITHUB_WORKSPACE/myconfig/config.ramips.2.kernel5.4 $GITHUB_WORKSPACE/myconfig/config.ramips.2.openwrt
 
 
      #   0a2ed285e4
