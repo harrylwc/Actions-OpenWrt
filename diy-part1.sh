5c1b4cbe88457d1d5e5f235f77eadaf3eafe4c86
Harry Lau <harrylwc@gmail.com>
2024-12-08 21:19:43 +0800

    modified:   diy-part1.sh 	modified:   diy-part2.sh 	new file:   patches/feeds.conf.default

--- diy-part1.sh
+++ diy-part1.sh
@@ -2,7 +2,7 @@ git clone https://github.com/x-wrt/x-wrt
 
 git clone https://github.com/coolsnowwolf/lede lede
 lede/scripts/feeds update -a
-
+cp patches/feeds.conf.default x-wrt/
 sudo cp copy_file.sh /usr/bin
 cp copy_file.sh x-wrt/
 cd x-wrt
