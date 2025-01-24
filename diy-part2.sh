14b21d418a4137e402b48856db186aaa2ed3f259
harrylwc <harrylwc@gmail.com>
2025-01-25 07:01:44 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -123,6 +123,7 @@ mv $GITHUB_WORKSPACE/patches/400-custom_hk-change-txpower-and-dfs.patch $GITHUB_
 cd $GITHUB_WORKSPACE/x-wrt/
 cd dl
 wget http://weike-iot.com:2211/rockchip/bsp/rk3568_OpenWRT/downloads/ddnsto-binary-3.0.4.tar.gz
+ls -alt ddnsto*
 cd ..
 mv package/feeds/video/wayland /tmp/
 
